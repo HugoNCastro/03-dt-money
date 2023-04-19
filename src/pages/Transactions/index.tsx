a1008564a9d259983a72c0b432e01c79ebf95318
HugoNCastro <hugonaaman@gmail.com>
2023-04-19 15:23:49 -0300

    Finaliza table e estilização

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,48 @@
 import { Header } from '../../components/Header'
 import { Summary } from '../../components/Summary'
+import { SearchForm } from './components/SearchForm'
+import {
+  PriceHighlight,
+  TransactionsContaier,
+  TransactionsTable,
+} from './styles'
 
 export function Transactions() {
   return (
     <div>
       <Header />
       <Summary />
+      <TransactionsContaier>
+        <SearchForm />
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight variant="income">R$ 12.000,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>19/04/2023</td>
+            </tr>
+            <tr>
+              <td>Hamburger</td>
+              <td>
+                <PriceHighlight variant="outcome">- R$ 100,00</PriceHighlight>
+              </td>
+              <td>Alimentação</td>
+              <td>19/04/2023</td>
+            </tr>
+            <tr>
+              <td>Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight variant="income">R$ 12.000,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>19/04/2023</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContaier>
     </div>
   )
 }
