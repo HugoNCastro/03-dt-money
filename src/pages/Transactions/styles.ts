c88ddb2e8b892c0974dab4e4b767eaab3416e038
HugoNCastro <hugonaaman@gmail.com>
2023-04-26 14:53:41 -0300

    Cria contexto e consome api para mostrar os dados

--- src/pages/Transactions/styles.ts
+++ src/pages/Transactions/styles.ts
@@ -1,6 +1,6 @@
 import styled from 'styled-components'
 
-export const TransactionsContaier = styled.main`
+export const TransactionsContainer = styled.main`
   width: 100%;
   max-width: 1120px;
   margin: 4rem auto 0;
